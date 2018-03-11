49f94e01e753dd0dc3bfdd4290b4706535f25ff7
Eduard Frigola Bagué <eduardfrigola@yahoo.com>
2018-03-11 19:24:44 +0100

    mapper node now maps vector<float> and due the fact that we have connections between vector<float> and float we are able to use it with either floats or vector<float>

--- src/Nodes/Default_Nodes/mapper.cpp
+++ src/Nodes/Default_Nodes/mapper.cpp
@@ -9,38 +9,27 @@
 #include "mapper.h"
 
 mapper::mapper() : ofxOceanodeNodeModel("Mapper"){
-    parameters->add(Input.set("Input", 0, 0.0, 1.0));
-    parameters->add(MinInput.set("MinInput", 0, 0.0, 1.0));
-    parameters->add(MaxInput.set("MaxInput", 1.0, 0.0, 1.0));
-    parameters->add(MinOutput.set("MinOutput", 0, 0.0, 1.0));
-    parameters->add(MaxOutput.set("MaxOutput", 1.0, 0.0, 1.0));
-    parameters->add(Output.set("Output", 0, 0.0, 1.0));
+    parameters->add(input.set("Input", {0}, {0}, {1}));
+    parameters->add(minInput.set("MinInput", 0, 0.0, 1.0));
+    parameters->add(maxInput.set("MaxInput", 1.0, 0.0, 1.0));
+    parameters->add(minOutput.set("MinOutput", 0, 0.0, 1.0));
+    parameters->add(maxOutput.set("MaxOutput", 1.0, 0.0, 1.0));
+    parameters->add(output.set("Output", {0}, {0}, {1}));
 
     
-    Input.addListener(this, &mapper::recalculate);
+    input.addListener(this, &mapper::recalculate);
 //    MinInput.addListener(this, &mapper::recalculate);
 //    MaxInput.addListener(this, &mapper::recalculate);
 //    MinOutput.addListener(this, &mapper::recalculate);
 //    MaxOutput.addListener(this, &mapper::recalculate);
 }
 
-float mapper::getRange(){
-    float f;
-    recalculate(f);
-    return(Output);
-}
-
-void mapper::recalculate(float& f)
+void mapper::recalculate(vector<float>& vf)
 {
-    Output = ofMap(Input,MinInput,MaxInput,MinOutput,MaxOutput, true);
-}
-    
-void mapper::resetRange()
-{
-    Input = 0.0;
-    MinInput = 0.0;
-    MaxInput = 1.0;
-    MinOutput = 0.0;
-    MaxOutput = 1.0;
+    vector<float> tempOut = vf;
+    for(auto &f : tempOut){
+       f = ofMap(f,minInput,maxInput,minOutput,maxOutput, true);
+    }
+    output = tempOut;
 }
 
