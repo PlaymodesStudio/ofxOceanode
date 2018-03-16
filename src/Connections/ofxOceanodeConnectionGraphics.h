5ccb368ec90f00abe83a1e6f31451138413b205e
Eloi Maduell <eloi@playmodes.com>
2018-03-16 18:00:33 +0100

    added drawing bezier curves for wires

--- src/Connections/ofxOceanodeConnectionGraphics.h
+++ src/Connections/ofxOceanodeConnectionGraphics.h
@@ -9,6 +9,8 @@
 #define ofxOceanodeConnectionGraphics_h
 
 #include "ofMain.h"
+#include "glm/gtx/vector_angle.hpp"
+
 
 class ofxOceanodeConnectionGraphics{
 public:
@@ -24,7 +26,39 @@ public:
         if(points[1] != glm::vec2(-1, -1)){
             ofPushMatrix();
             ofMultMatrix(transformationMatrix->get());
-            ofDrawLine(points[0], points[1]);
+            {
+                
+//                ofDrawLine(points[0], points[1]);
+
+                ofPath wirePath;
+                wirePath.setCurveResolution(64);
+                wirePath.setFilled(false);
+                wirePath.setStrokeWidth(2);
+                wirePath.setStrokeColor(ofColor::black);
+                
+                glm::vec2  controlPoint;
+                glm::vec2  endControl;
+                
+                float dist = glm::distance(points[0],points[1]);
+                
+                
+                controlPoint.x = ofMap(dist,0,1500,25,400);
+                controlPoint.y = 0;
+
+                wirePath.clear();
+                wirePath.moveTo(points[0].x,points[0].y);
+
+                wirePath.bezierTo(points[0].x+controlPoint.x,points[0].y+controlPoint.y,
+                                    points[1].x-controlPoint.x,points[1].y-controlPoint.y,
+                                    points[1].x, points[1].y);
+                wirePath.moveTo(points[1].x,points[1].y);
+                
+                ofSetColor(0,0,0);
+                wirePath.draw();
+//                wirePath.translate(glm::vec2(0,-3));
+//                wirePath.draw();
+                
+            }
             ofPopMatrix();
         }
     }
