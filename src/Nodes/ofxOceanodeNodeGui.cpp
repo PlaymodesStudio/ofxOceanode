2c0f105627001e51bcd668aadf67dca1c5b72898
Eduard Frigola Bagué <eduardfrigola@yahoo.com>
2018-07-19 18:38:37 +0200

    fix precision in multislider

--- src/Nodes/ofxOceanodeNodeGui.cpp
+++ src/Nodes/ofxOceanodeNodeGui.cpp
@@ -163,7 +163,7 @@ void ofxOceanodeNodeGui::updateGui(){
                     dropdown->select(val);
                 }));
             }else if(absParam.type() == typeid(ofParameter<vector<float>>).name()){
-                gui->addMultiSlider(absParam.cast<vector<float>>());
+                gui->addMultiSlider(absParam.cast<vector<float>>())->setPrecision(1000);
             }else if(absParam.type() == typeid(ofParameter<vector<int>>).name()){
                 gui->addMultiSlider(absParam.cast<vector<int>>());
             }else {
