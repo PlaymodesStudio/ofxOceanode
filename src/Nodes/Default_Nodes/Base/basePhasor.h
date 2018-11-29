f17ef607dbd35280004e4c13a5685cb88e3b1084
eduardfrigola <eduardfrigola@yahoo.com>
2018-11-29 15:46:25 +0100

    Phasor now can generate multiple ouputs given an array of beatdivs or beatmults

--- src/Nodes/Default_Nodes/Base/basePhasor.h
+++ src/Nodes/Default_Nodes/Base/basePhasor.h
@@ -10,32 +10,62 @@
 #define basePhasor_h
 #include "ofMain.h"
 
-class basePhasor:public ofThread{
+class basePhasor: public ofThread{
 public:
     basePhasor();
     ~basePhasor();
     
-    float  bpm_Param;
-    float    beatsMult_Param;
-    float    beatsDiv_Param;
+    float    bpm_Param;
+    vector<float>    beatsMult_Param;
+    vector<float>    beatsDiv_Param;
     float  initPhase_Param;
     bool   loop_Param;
 
-    float getPhasor();
+    vector<float> getPhasors();
+    float getPhasor(){
+        return getPhasors()[0];
+    }
     void  resetPhasor();
     
     ofEvent<void> phasorCycle;
     
+    void checkChangedSize(){
+        if(beatsMult_Param.size() != 1 && beatsMult_Param.size() != numPhasors){
+            resizePhasors(beatsMult_Param.size());
+        }else if(beatsDiv_Param.size() != 1 && beatsDiv_Param.size() != numPhasors){
+            resizePhasors(beatsDiv_Param.size());
+        }else if(beatsDiv_Param.size() == 1 && beatsMult_Param.size() == 1 && numPhasors != 1){
+            resizePhasors(1);
+        }
+    }
     
 private:
+    
+    void resizePhasors(int n){
+        numPhasors = n;
+        phasor.resize(n);
+        phasorMod.resize(n);
+        resetPhasor();
+    }
+    
+    float getValueForIndex(vector<float> &vf, int i){
+        if(i < vf.size()){
+            return vf[i];
+        }else{
+            return vf[0];
+        }
+    }
+    
+    int numPhasors;
+    
     ofTimer timer;
     
     void threadedFunction() override;
 
-    double  phasor;
-    double  phasorMod;
-    ofThreadChannel<double> phasorToSend;
-    double momentaryPhasor;
+    vector<double>  phasor;
+    vector<double>  phasorMod;
+    ofThreadChannel<vector<double>> phasorToSend;
+    vector<double> momentaryPhasor;
 };
 
 #endif /* basePhasor_h */
--- src/Nodes/Default_Nodes/Generators/phasor.cpp
+++ src/Nodes/Default_Nodes/Generators/phasor.cpp
@@ -16,23 +16,25 @@ phasor::phasor() : ofxOceanodeNodeModel("Phasor")
     parameterAutoSettersListeners.push(initPhase_Param.newListener([&](float &val){
         basePh.initPhase_Param = val+phaseOffset - int(val+phaseOffset);
     }));
-    parameterAutoSettersListeners.push(beatsMult_Param.newListener([&](float &val){
+    parameterAutoSettersListeners.push(beatsMult_Param.newListener([&](vector<float> &val){
         basePh.beatsMult_Param = val;
+        basePh.checkChangedSize();
     }));
-    parameterAutoSettersListeners.push(beatsDiv_Param.newListener([&](float &val){
+    parameterAutoSettersListeners.push(beatsDiv_Param.newListener([&](vector<float> &val){
         basePh.beatsDiv_Param = val;
+        basePh.checkChangedSize();
     }));
     parameterAutoSettersListeners.push(loop_Param.newListener([&](bool &val){
         basePh.loop_Param = val;
     }));
 
     addParameterToGroupAndInfo(bpm_Param.set("BPM", 120, 0, 999)).isSavePreset = false;
-    parameters->add(beatsDiv_Param.set("Beats Div", 2, 1, 512));
-    parameters->add(beatsMult_Param.set("Beats Mult", 1, 1, 512));
+    parameters->add(beatsDiv_Param.set("Beats Div", {2}, {1}, {512}));
+    parameters->add(beatsMult_Param.set("Beats Mult", {1}, {1}, {512}));
     parameters->add(initPhase_Param.set("Initial Phase", 0, 0, 1));
     parameters->add(resetPhase_Param.set("Reset Phase"));
     parameters->add(loop_Param.set("Loop", true));
-    addOutputParameterToGroupAndInfo(phasorMonitor.set("Phasor Output", 0, 0, 1));
+    addOutputParameterToGroupAndInfo(phasorMonitor.set("Phasor Output", {0}, {0}, {1}));
     
     resetPhaseListener = resetPhase_Param.newListener([&](){
         basePh.resetPhasor();
@@ -41,7 +43,7 @@ phasor::phasor() : ofxOceanodeNodeModel("Phasor")
 
 void phasor::update(ofEventArgs &e)
 {
-    phasorMonitor = basePh.getPhasor();
+    phasorMonitor = basePh.getPhasors();
 }
 
 void phasor::setPhase(float _phase){
