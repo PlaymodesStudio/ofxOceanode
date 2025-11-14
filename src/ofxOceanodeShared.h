c74d98bdcda58cbb014e6f2cdac5dcf6911661b7
eloimaduell <eloi@playmodes.com>
2025-11-14 23:49:09 +0100

    [Shared] : added getCurrentBankName() and setCurrentBankName() and minor changes on the initializations of the needed vars

--- src/ofxOceanodeShared.h
+++ src/ofxOceanodeShared.h
@@ -192,6 +192,14 @@ public:
 		getInstance().currentPresetName = presetName;
 	}
 	
+	static string getCurrentBankName(){
+		return getInstance().currentBankName;
+	}
+	
+	static void setCurrentBankName(const string& bankName){
+		getInstance().currentBankName = bankName;
+	}
+
 	static std::string getCurrentPresetPath(){
 		return getInstance().currentPresetPath;
 	}
@@ -226,20 +234,21 @@ private:
     unsigned int centralNode_id = 0;
     unsigned int leftNode_id = 0;
 	
-    std::shared_ptr<macroCategory> macroDirectoryStructure;
-	std::string currentPresetName = "";
-	
+    shared_ptr<macroCategory> macroDirectoryStructure;
 	ofEvent<string> macroUpdatedEvent;
-    bool presetLoading;
+    
+	bool presetLoading;
     ofEvent<void> presetWillBeLoadedEvent;
     ofEvent<void> presetHasLoadedEvent;
-	
+	string currentPresetName = "";
+	string currentPresetPath = "";
+	string currentBankName = "";
+
 	vector<abstractPortal*> portals;
     vector<abstractPortal*> currentUpdatingPortals;
     
     ofxOceanodeConfigurationFlags configurationFlags;
 	
-	std::string currentPresetPath = "";
 
 };
 
